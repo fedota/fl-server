616d94cf50a4635ba8cbab350021b60fb0ccb23a
Shashank P <1997shashankp@gmail.com>
2019-09-04 22:39:44 +0530

    execution of federated averaging python script from server

--- server/federated_averaging.py
+++ server/federated_averaging.py
@@ -5,110 +5,125 @@ import tensorflow as tf
 import numpy as np
 import keras
 from keras.models import load_model
+import os
+
 
 def federated_averaging(updates, model_path, ckpt_path):
-    
+
+    print("Model path: ", model_path)
+    print("Checkpoint path: ", ckpt_path)
+    print("Updates array: ", updates)
+
     total_num_batches = 0
-    
+
     # Load model architecture
     average_weight_updates = load_model(model_path)
     sum_weight_updates = load_model(model_path)
     device_weight_updates = load_model(model_path)
-    
+
     # Calculate sum of weight updates from all devices
     for device_index in range(len(updates)):
-        
+
         n, weight_updates_path = updates[device_index]
-        
+
         total_num_batches += n
-        
+
         if device_index == 0:
-             sum_weight_updates.load_weights(weight_updates_path)
-                
+            sum_weight_updates.load_weights(weight_updates_path)
+
         else:
             # Load device weight updates checkpoint
             device_weight_updates.load_weights(weight_updates_path)
-            
+
             # Add weight updates from device to prefix sum
             for layer_index in range(len(sum_weight_updates.layers)):
 
                 # Old sum of weight updates
-                old_sum_weight_updates_values = sum_weight_updates.layers[layer_index].get_weights()
+                old_sum_weight_updates_values = sum_weight_updates.layers[layer_index].get_weights(
+                )
 
-                # Device weight updates      
-                device_weight_updates_values = device_weight_updates.layers[layer_index].get_weights()
+                # Device weight updates
+                device_weight_updates_values = device_weight_updates.layers[layer_index].get_weights(
+                )
 
                 # Weight updates calculation
-                sum_weight_updates.layers[layer_index].set_weights(np.asarray(old_sum_weight_updates_values) 
-                                                                 + np.asarray(device_weight_updates_values))
+                sum_weight_updates.layers[layer_index].set_weights(np.asarray(old_sum_weight_updates_values)
+                                                                   + np.asarray(device_weight_updates_values))
 
 #                print("old weights: ",  old_layer_weights)
 #                print("new weights: ",  new_layer_weights)
 #                print("update weights: ",  update_weights.layers[i].get_weights())
 
-    
     # Calculate average
     for layer_index in range(len(sum_weight_updates.layers)):
 
         # Value of sum of weight updates
-        sum_weight_updates_values = sum_weight_updates.layers[layer_index].get_weights()
+        sum_weight_updates_values = sum_weight_updates.layers[layer_index].get_weights(
+        )
 
         # Calculate average and store
-        average_weight_updates.layers[layer_index].set_weights( np.asarray(sum_weight_updates_values)/total_num_batches )
+        average_weight_updates.layers[layer_index].set_weights(
+            np.asarray(sum_weight_updates_values)/total_num_batches)
 
 #        print("old weights: ",  old_layer_weights)
 #        print("new weights: ",  new_layer_weights)
 #        print("update weights: ",  update_weights.layers[i].get_weights())
-            
+
     # Add average of weight updates to checkpoint
     # Load model and checkpoints
     model = load_model(model_path)
     model.load_weights(ckpt_path)
-    
+
     for layer_index in range(len(model.layers)):
-        
+
         # Average of weight updates values
-        average_weight_updates_values = average_weight_updates.layers[layer_index].get_weights()
-        
+        average_weight_updates_values = average_weight_updates.layers[layer_index].get_weights(
+        )
+
         old_model_values = model.layers[layer_index].get_weights()
-        
-        model.layers[layer_index].set_weights(np.asarray(old_model_values) + np.asarray(average_weight_updates_values))
-    
+
+        model.layers[layer_index].set_weights(np.asarray(
+            old_model_values) + np.asarray(average_weight_updates_values))
+
     # Save updated model checkpoints
     model.save_weights(ckpt_path)
+    print("New checkpoint saved at ", ckpt_path)
 
 def main():
-	# define Arguments
-	parser = argparse.ArgumentParser(description='Perform Federated Averaging')
-	parser.add_argument("--cf", "--ckpt-file-path", required=True, nargs=1)
-	parser.add_argument("--mf", "--model-file-path", required=True, nargs=1)
-	parser.add_argument("--u", "--updates", required=True, nargs='*')
+        # define Arguments
+    parser = argparse.ArgumentParser(description='Perform Federated Averaging')
+    parser.add_argument("--cf", "--ckpt-file-path", required=True, nargs=1)
+    parser.add_argument("--mf", "--model-file-path", required=True, nargs=1)
+    parser.add_argument("--u", "--updates", required=True, nargs='*')
 
-	# params for federated averaging
-	model_path = ''
-	ckpt_path = ''
-	updates = []
+    # params for federated averaging
+    model_path = ''
+    ckpt_path = ''
+    updates = []
 
-	# parse arguments
-	args = parser.parse_args()
-	for arg in vars(args):
-		print(arg, getattr(args, arg))
-		if (arg == "mf"):
-			model_path = getattr(args, arg)
-		elif (arg == "cf"):
-			ckpt_path = getattr(args, arg)
-		else:
-			update_args = getattr(args, arg)
-			print(update_args)
-			for i in range(len(update_args), 2):
-				print(update_args[i])
-				print(update_args[i + 1])
-				n = int(update_args[i])
-				path = update_args[i + 1]
-				updates.append((n, path))
-	
-	# run federated averaging
-	federated_averaging(updates, model_path, ckpt_path)
+    # parse arguments
+    args = parser.parse_args()
+    for arg in vars(args):
+                # print(arg, getattr(args, arg))
+        if (arg == "mf"):
+            model_path = getattr(args, arg)[0]
+        elif (arg == "cf"):
+            ckpt_path = getattr(args, arg)[0]
+        else:
+            update_args = getattr(args, arg)
+            print(update_args)
+            print("Len: ", len(update_args))
+            for i in range(0, len(update_args), 2):
+                print("index: ", i)
+                # print(update_args[i])
+                # print(update_args[i + 1])
+                n = int(update_args[i])
+                path = update_args[i + 1]
+                updates.append((n, path))
+
+    # run federated averaging
+    federated_averaging(updates, model_path, ckpt_path)
+
 
 if __name__ == "__main__":
     main()
--- server/main.go
+++ server/main.go
@@ -1,60 +1,60 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net"
 	"os"
-	"io"
-	"sync"
-	"strconv"
-	"time"
 	"os/exec"
+	"strconv"
+	"sync"
+	"time"
+
+	pb "fl-server/server/genproto"
 
 	"google.golang.org/grpc"
-	pb "fl-server/server/genproto"
 )
 
 // constants
 const (
-	port = ":50051"
-	modelFilePath = "./model/model.h5"
-	checkpointFilePath = "./data/fl_checkpoint"
+	port                  = ":50051"
+	modelFilePath         = "./model/model.h5"
+	checkpointFilePath    = "./data/fl_checkpoint"
 	updatedCheckpointPath = "./data/fl_checkpoint_update_"
-	chunkSize = 64 * 1024
-	reconnectionTime = 8000
-	estimatedRoundTime = 8000
+	chunkSize             = 64 * 1024
+	reconnectionTime      = 8000
+	estimatedRoundTime    = 8000
 )
 
 type flRoundClientResult struct {
-	checkpointWeight int64
+	checkpointWeight   int64
 	checkpointFilePath string
 }
 
-// server struct to implement gRPC Round service interface 
+// server struct to implement gRPC Round service interface
 type server struct {
-	numCheckIns int
-	numUpdates int
-	mu  sync.Mutex
+	numCheckIns       int
+	numUpdates        int
+	mu                sync.Mutex
 	checkpointUpdates map[int]flRoundClientResult
 }
-
 
 func main() {
 	// listen
 	lis, err := net.Listen("tcp", port)
-	check(err, "Failed to listen on port" + port)
+	check(err, "Failed to listen on port"+port)
 
 	// register FL round server
 	srv := grpc.NewServer()
 	// impl instance
-	flServer := &server {numCheckIns: 0, checkpointUpdates: make(map[int]flRoundClientResult)}
+	flServer := &server{numCheckIns: 0, checkpointUpdates: make(map[int]flRoundClientResult)}
 	pb.RegisterFlRoundServer(srv, flServer)
 
 	go flServer.EventLoop()
 
 	// start serving
 	err = srv.Serve(lis)
-	check(err, "Failed to serve on port" + port)
+	check(err, "Failed to serve on port"+port)
 
 }
 
@@ -64,11 +64,11 @@ func main() {
 func (s *server) CheckIn(stream pb.FlRound_CheckInServer) error {
 
 	var (
-		buf		[] byte
-		n		int
-		file	*os.File
+		buf  []byte
+		n    int
+		file *os.File
 	)
-	
+
 	// receive check-in request
 	checkinReq, err := stream.Recv()
 	log.Println("Client Name: ", checkinReq.Message)
@@ -79,14 +79,14 @@ func (s *server) CheckIn(stream pb.FlRound_CheckInServer) error {
 	s.numCheckIns++
 	s.mu.Unlock()
 	log.Println("Count: ", s.numCheckIns)
-	
+
 	// open file
 	file, err = os.Open(checkpointFilePath)
 	check(err, "Could not open checkpoint file")
 	defer file.Close()
 
 	// make a buffer of a defined chunk size
-	buf  = make([]byte, chunkSize)
+	buf = make([]byte, chunkSize)
 
 	for {
 		// read the content (by using chunks)
@@ -96,7 +96,7 @@ func (s *server) CheckIn(stream pb.FlRound_CheckInServer) error {
 		}
 		check(err, "Could not read file content")
 
-		// send the FL checkpoint Data (file chunk + type: FL checkpoint) 
+		// send the FL checkpoint Data (file chunk + type: FL checkpoint)
 		err = stream.Send(&pb.FlData{
 			Message: &pb.Chunk{
 				Content: buf[:n],
@@ -134,11 +134,11 @@ func (s *server) Update(stream pb.FlRound_UpdateServer) error {
 		if err == io.EOF {
 			return stream.SendAndClose(&pb.FlData{
 				IntVal: reconnectionTime,
-				Type: pb.Type_FL_RECONN_TIME,
+				Type:   pb.Type_FL_RECONN_TIME,
 			})
 		}
 		check(err, "Unable to receive update data from client")
-		
+
 		if flData.Type == pb.Type_FL_CHECKPOINT_UPDATE {
 			// write data to file
 			_, err = file.Write(flData.Message.Content)
@@ -148,7 +148,7 @@ func (s *server) Update(stream pb.FlRound_UpdateServer) error {
 			// TODO: modify by making a go-routine to do updates
 			s.mu.Lock()
 			s.checkpointUpdates[index] = flRoundClientResult{
-				checkpointWeight: flData.IntVal,
+				checkpointWeight:   flData.IntVal,
 				checkpointFilePath: filePath,
 			}
 			s.mu.Unlock()
@@ -159,37 +159,51 @@ func (s *server) Update(stream pb.FlRound_UpdateServer) error {
 
 func (s *server) EventLoop() {
 	// TODO: change this naive way
-	// Wait for an estimated time for round 
+	// Wait for an estimated time for round
 	log.Println("Event Loop in sleep")
 	time.Sleep(estimatedRoundTime * time.Millisecond)
 	log.Println("Event loop out of sleep")
 
+	s.FederatedAveraging()
+
+	log.Println("Federated Averaging completed")
+
+
 	// or compare with some base number
 	// TODO: change else condition
-	if s.numUpdates == s.numCheckIns && s.numUpdates > 0 {
-		// assumed no more updates and check-ins
-		s.FederatedAveraging()
-	} else {
-		log.Fatalf("Could not complete round")
-	} 
+	// if s.numUpdates == s.numCheckIns && s.numUpdates > 0 {
+	// 	// assumed no more updates and check-ins
+	// 	s.FederatedAveraging()
+	// } else {
+	// 	log.Fatalf("Could not complete round")
+	// }
 }
 
-// launch the fede
+// Runs federated averaging
 func (s *server) FederatedAveraging() {
 	// determine arguments
 	args := ""
 	for _, v := range s.checkpointUpdates {
 		args += strconv.FormatInt(v.checkpointWeight, 10) + " " + v.checkpointFilePath + " "
 	}
-	// log.Println("Arguments: ", args)
+
+	argsList := []string{"federated_averaging.py", "--cf", checkpointFilePath, "--mf", modelFilePath, "--u", "281", "./device1/weight_updates", "281", "./device2/weight_updates", "296", "./device3/weight_updates"}
+	// if args == "" {
+	// 	return
+	// }
+
+	log.Println("Arguments: ", args)
 	// model path
-	err := exec.Command("python ./federated_averaging.py", "--cf" + checkpointFilePath, "--mf" + modelFilePath, "--u" + args).Run()
+	cmd := exec.Command("python", argsList...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
 	check(err, "Unable to run federated averaging")
 }
 
 // Check for error, log and exit if err
 func check(err error, errorMsg string) {
 	if err != nil {
-		log.Fatalf(errorMsg + " ==> ", err)
+		log.Fatalf(errorMsg+" ==> ", err)
 	}
 }
